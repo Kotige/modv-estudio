b55ab311bd7075c6d46b4c59eb70d1a46e9ed4b3
Vitor <vitor_fernandes_pie@yahoo.com.br>
2025-10-20 18:50:33 -0300

    feat: carousel implementation

---
+++ src/js/carousel.js
@@ -0,0 +1,40 @@
+import img1 from '../assets/images/logo.png';
+import img2 from '../assets/images/faixa.png';
+import img3 from '../assets/images/logo.png';
+import img4 from '../assets/images/faixa.png';
+import img5 from '../assets/images/logo.png';
+
+export function initCarousel() {
+    const images = [
+        { src: img1, link: "#" },
+        { src: img2, link: "#" },
+        { src: img3, link: "#" },
+        { src: img4, link: "#" },
+        { src: img5, link: "#" }
+    ];
+
+    let currentIndex = 0;
+
+    const mainImg = document.querySelector(".main-img");
+    const mainLink = document.querySelector("#main-link");
+    const smallImgs = document.querySelectorAll(".small-img");
+    const nextBtn = document.querySelector("#next-btn");
+
+    function updateCarousel() {
+        const main = images[currentIndex];
+        const next1 = images[(currentIndex + 1) % images.length];
+        const next2 = images[(currentIndex + 2) % images.length];
+
+        mainImg.src = main.src;
+        mainLink.href = main.link;
+        smallImgs[0].src = next1.src;
+        smallImgs[1].src = next2.src;
+    }
+
+    nextBtn.addEventListener("click", () => {
+        currentIndex = (currentIndex + 1) % images.length;
+        updateCarousel();
+    });
+
+    updateCarousel();
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,11 @@
 // Import our custom CSS
 import '../scss/styles.scss'
 
+import {initCarousel} from './carousel.js';
+
 // Import all of Bootstrap’s JS
 import * as bootstrap from 'bootstrap'
+
+document.addEventListener("DOMContentLoaded", () => {
+    initCarousel();
+})
