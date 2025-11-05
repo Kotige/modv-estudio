b98d3bab111b653acf1d44234c8cdedb84257be1
Vitor <vitor_fernandes_pie@yahoo.com.br>
2025-11-05 07:52:11 -0300

    chrore: page footers corrections

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,19 @@ export default {
     root: resolve(__dirname, 'src'),
     base: './',
     build: {
-    outDir: '../dist'
+    outDir: '../dist',
+    rollupOptions: {
+        input: {
+            main: resolve(__dirname, 'src/index.html'),
+            fucinn: resolve(__dirname, 'src/fucinn.html'),
+            inove: resolve(__dirname, 'src/inove.html'),
+            lavinia: resolve(__dirname, 'src/lavinia.html'),
+            livia: resolve(__dirname, 'src/livia.html'),
+            teteu: resolve(__dirname, 'src/teteu.html'),
+            tiebreak: resolve(__dirname, 'src/tiebreak.html'),
+            vital: resolve(__dirname, 'src/vital.html')
+        }
+    }
     },
     server: {
     port: 5173
